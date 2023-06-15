f286cdff0956c58e518ddce6d48da87ea9479a2a
Anvid31 <mendozadiazjuandavid@gmail.com>
2023-06-15 09:03:18 -0500

    First

---
+++ components/footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      
+
+      <div class="box">
+         <h3>Links Rapidos</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Registro</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Carrito</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Ordenes</a>
+      </div>
+
+      <div class="box">
+         <h3>Contactame</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +57 3233866344</a>
+         <a href="mailto:mendozadiazjuandavid.com"><i class="fas fa-envelope"></i> mendozadiazjuandavid@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Cucuta, Colombia     </a>
+      </div>
+
+      <div class="box">
+         <h3>Redes</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> | all rights reserved!</div>
+
+</footer>
